fb732d379bf9f6a5475c033a1c85f4c788dab405
san-ar <sanar.debnath.tc@gmail.com>
2024-03-16 00:16:44 +0800

    Updated SharedPreferences to save "Save Icon" state

--- app/src/main/java/com/example/all_menu/adapters/MenuVerAdapter.java
+++ app/src/main/java/com/example/all_menu/adapters/MenuVerAdapter.java
@@ -2,6 +2,7 @@ package com.example.all_menu.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,6 +27,7 @@ public class MenuVerAdapter extends RecyclerView.Adapter<MenuVerAdapter.ViewHold
     List<MenuVerModel> list;
     List<MenuVerModel> savedMealsList;
     private final MenuActivity menuActivity;
+    private static final String ICON_STATE_KEY = "icon_state";
 
 
     // Constructor to initialize the adapter with context and menu list
@@ -57,42 +59,43 @@ public class MenuVerAdapter extends RecyclerView.Adapter<MenuVerAdapter.ViewHold
         // If the meal is saved
         boolean isSaved = isMealSaved(item);
 
-        // The save icon based on the save state
-        if (isSaved) {
-            holder.iv_save_icon.setImageResource(R.drawable.saved_icon);
-        } else {
-            holder.iv_save_icon.setImageResource(R.drawable.save_icon);
-        }
+        // Update save icon state
+        updateIconState(holder.iv_save_icon, item);
 
         // Click listener for the save icon
-        holder.iv_save_icon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Toggle the save state
-                if (isSaved) {
-                    savedMealsList.remove(item);// Remove the meal from the saved list
-                    menuActivity.removeMealFromSharedPreferences(item);// Remove the meal from the SharedPreferences
-                    Toast.makeText(context, "Item removed", Toast.LENGTH_SHORT).show();
-                } else {
-                    savedMealsList.add(item); // Add the meal to the saved list
-                    menuActivity.saveMealToSharedPreferences(item); // Save the meal to SharedPreferences
-                    Toast.makeText(context, "Item saved", Toast.LENGTH_SHORT).show();
-                }
-                // Notify the adapter that the data set has changed
-                notifyDataSetChanged();
+        holder.iv_save_icon.setOnClickListener(v -> {
+
+            // Toggle the save state
+            boolean isIconSaved = isMealSaved(item);
+            isIconSaved = !isIconSaved;
+            // Save icon state to SharedPreferences
+            SharedPreferences.Editor editor = context.getSharedPreferences("IconState", Context.MODE_PRIVATE).edit();
+            editor.putBoolean(ICON_STATE_KEY + item.getTv_meal_title(), isIconSaved);
+            editor.apply();
+            // Update icon state
+            updateIconState(holder.iv_save_icon, item);
+
+            // Handle saving/removing meal
+            if (isSaved) {
+                savedMealsList.remove(item);// Remove the meal from the saved list
+                menuActivity.removeMealFromSharedPreferences(item);// Remove the meal from the SharedPreferences
+                Toast.makeText(context, "Item removed", Toast.LENGTH_SHORT).show();
+            } else {
+                savedMealsList.add(item); // Add the meal to the saved list
+                menuActivity.saveMealToSharedPreferences(item); // Save the meal to SharedPreferences
+                Toast.makeText(context, "Item saved", Toast.LENGTH_SHORT).show();
             }
+            // Notify the adapter that the data set has changed
+            notifyDataSetChanged();
         });
 
 
         // Check if the meal title is "Yogurt Parfait"
         if (item.getTv_meal_title().equals("Yogurt Parfait")) {
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Start the Yogurt Parfait Meal Details activity when clicked
-                    Intent intent = new Intent(context, YogurtDetailsActivity.class);
-                    context.startActivity(intent);
-                }
+            holder.itemView.setOnClickListener(v -> {
+                // Start the Yogurt Parfait Meal Details activity when clicked
+                Intent intent = new Intent(context, YogurtDetailsActivity.class);
+                context.startActivity(intent);
             });
         }
 
@@ -139,6 +142,17 @@ public class MenuVerAdapter extends RecyclerView.Adapter<MenuVerAdapter.ViewHold
             time = itemView.findViewById(R.id.tv_time);
             meal_title = itemView.findViewById(R.id.tv_meal_title);
             iv_save_icon = itemView.findViewById(R.id.iv_save_icon);
+        }
+    }
+
+    public void updateIconState(ImageView saveIcon, MenuVerModel item){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("IconState", Context.MODE_PRIVATE);
+        boolean isIconSaved = sharedPreferences.getBoolean(ICON_STATE_KEY + item.getTv_meal_title(), false);
+
+        if(isIconSaved){
+            saveIcon.setImageResource(R.drawable.saved_icon);
+        } else {
+            saveIcon.setImageResource(R.drawable.save_icon);
         }
     }
 }
